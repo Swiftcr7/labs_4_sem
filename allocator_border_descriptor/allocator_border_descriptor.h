3fe8fad9209471fc681077e988dda67fcd12c7b0
swiftcr7 <dimkafedulov@yandex.ru>
2023-05-21 03:42:10 +0300

    allocator_border_descriptor: try the git

---
+++ allocator_border_descriptor/allocator_border_descriptor.h
@@ -0,0 +1,159 @@
+#ifndef LABS_4_SEM_ALLOCATOR_BORDER_DESCRIPTOR_H
+#define LABS_4_SEM_ALLOCATOR_BORDER_DESCRIPTOR_H
+
+#include <iostream>
+#include "../../untitled4/allocator2.h"   //TODO: fix path!!!!!
+
+class allocator_border_descriptor final : public memory{
+private:
+    void* _global_memory;
+public:
+    enum class detour{
+        first,
+        best,
+        worst
+    };
+private:
+
+    void** get_pointer_to_next_block(void* block) const{
+        return reinterpret_cast<void**>(reinterpret_cast<size_t*>(block)+1);
+    }
+
+    void** get_pointer_to_past_block(void* block) const {
+        return reinterpret_cast<void**>(reinterpret_cast<void**>(reinterpret_cast<size_t*>(block)+1)+1);
+    }
+
+
+
+    size_t* get_size()const{
+        return reinterpret_cast<size_t*>(reinterpret_cast<detour*>(reinterpret_cast<logger**>(reinterpret_cast<memory**>(_global_memory)+1)+1)+1);
+    }
+
+    memory** get_allocator()const{
+        return reinterpret_cast<memory**>(_global_memory);
+    }
+
+    logger** get_logger()const{
+        return reinterpret_cast<logger**>(reinterpret_cast<memory**>(_global_memory)+1);
+    }
+
+    allocator_border_descriptor::detour* get_detour()const{
+        return reinterpret_cast<detour*>(reinterpret_cast<logger**>(reinterpret_cast<memory**>(_global_memory)+1)+1);
+    }
+
+    void** get_pointer()const{
+        return reinterpret_cast<void**>(reinterpret_cast<size_t*>(reinterpret_cast<detour*>(reinterpret_cast<logger**>(reinterpret_cast<memory**>(_global_memory)+1)+1)+1)+1);
+    }
+
+
+    size_t* get_block_size(void* block) const{
+        return reinterpret_cast<size_t*>(block);
+    }
+
+
+public:
+    explicit allocator_border_descriptor(std::size_t size, logger* my_logger = nullptr, memory* alloc = nullptr, allocator_border_descriptor::detour det=detour::first){
+        if (alloc == nullptr){
+            _global_memory = :: operator new(size+sizeof(logger*)+sizeof(size_t)+sizeof(void*) +sizeof(memory*)+sizeof(detour));
+        }else{
+            _global_memory=alloc->allocate(size+sizeof(logger*)+sizeof(size_t)+sizeof(void*) +sizeof(memory*)+sizeof(detour));
+        }
+        *reinterpret_cast<memory**>(_global_memory)=alloc;
+        *reinterpret_cast<logger**>(reinterpret_cast<memory**>(_global_memory)+1) = my_logger;
+        *reinterpret_cast<detour*>(reinterpret_cast<logger**>(reinterpret_cast<memory**>(_global_memory)+1)+1)=det;
+        *reinterpret_cast<size_t*>(reinterpret_cast<detour*>(reinterpret_cast<logger**>(reinterpret_cast<memory**>(_global_memory)+1)+1)+1)=size;
+        *reinterpret_cast<void**>(reinterpret_cast<size_t*>(reinterpret_cast<detour*>(reinterpret_cast<logger**>(reinterpret_cast<memory**>(_global_memory)+1)+1)+1)+1)= nullptr;
+    }
+    void* allocate(size_t target_size)const override{
+        if (*get_pointer()== nullptr){
+            if (*get_size()>=target_size+sizeof(size_t)+sizeof(void*)+sizeof(void*)){
+                auto bloc=reinterpret_cast<void*>(get_pointer()+1);
+                *get_block_size(bloc)=target_size;
+                *get_pointer_to_past_block(bloc)= nullptr;
+                *get_pointer_to_next_block(bloc)= nullptr;
+                *get_pointer()=bloc;
+                return reinterpret_cast<void*>(get_pointer_to_past_block(bloc)+1);
+            }else{
+                return nullptr;
+            }
+        }
+        auto current_block= *get_pointer();
+        if (reinterpret_cast<unsigned char*>(*get_pointer())-reinterpret_cast<unsigned char*>(reinterpret_cast<void**>(get_pointer())+1)>=target_size+sizeof(size_t)+2*sizeof(void*)){
+            auto bloc=reinterpret_cast<void *>(reinterpret_cast<void**>(get_pointer())+1);
+            *get_block_size(bloc)=target_size;
+            *get_pointer_to_past_block(bloc)= nullptr;
+            *get_pointer_to_past_block(*get_pointer())=bloc;
+            *get_pointer_to_next_block(bloc)=*get_pointer();
+            *get_pointer()=bloc;
+            return reinterpret_cast<void*>(get_pointer_to_past_block(bloc)+1);
+
+        }
+        while (*get_pointer_to_next_block(current_block)!= nullptr){
+            if (reinterpret_cast<unsigned char*>(*get_pointer_to_next_block(current_block))-(reinterpret_cast<unsigned char*>(get_pointer_to_past_block(current_block)+1)+(*get_block_size(current_block)))>=target_size+sizeof(size_t)+2*sizeof(void*)){
+                auto pointer=*get_pointer_to_next_block(current_block);
+                *get_block_size(reinterpret_cast<unsigned char*>(get_pointer_to_past_block(current_block)+1)+(*get_block_size(current_block)))=target_size;
+                *get_pointer_to_next_block(reinterpret_cast<unsigned char*>(get_pointer_to_past_block(current_block)+1)+*get_block_size(current_block))=*get_pointer_to_next_block(current_block);
+                *get_pointer_to_past_block(reinterpret_cast<unsigned char*>(get_pointer_to_past_block(current_block)+1)+*get_block_size(current_block))=current_block;
+                *get_pointer_to_next_block(current_block)=reinterpret_cast<unsigned char*>(get_pointer_to_past_block(current_block)+1)+*get_block_size(current_block);
+                *get_pointer_to_past_block(pointer)=reinterpret_cast<unsigned char*>(get_pointer_to_past_block(current_block)+1)+*get_block_size(current_block);
+                return reinterpret_cast<void*>(get_pointer_to_past_block(reinterpret_cast<unsigned char*>(get_pointer_to_past_block(current_block)+1)+*get_block_size(current_block))+1);
+            }else{
+                current_block=*get_pointer_to_next_block(current_block);
+            }
+        }
+        if (*get_pointer_to_next_block(current_block)== nullptr && (reinterpret_cast<unsigned char*>(reinterpret_cast<void**>(get_pointer())+1)+*get_size()-reinterpret_cast<unsigned char*>(get_pointer_to_past_block(current_block)+1)-*get_block_size(current_block))){
+            *get_block_size((reinterpret_cast<unsigned char*>(get_pointer_to_past_block(current_block)+1)+*get_block_size(current_block)))=target_size;
+            *get_pointer_to_next_block((reinterpret_cast<unsigned char*>(get_pointer_to_past_block(current_block)+1)+*get_block_size(current_block)))=nullptr;
+            *get_pointer_to_past_block((reinterpret_cast<unsigned char*>(get_pointer_to_past_block(current_block)+1))+*get_block_size(current_block))=current_block;
+            *get_pointer_to_next_block(current_block)=reinterpret_cast<unsigned char*>(get_pointer_to_past_block(current_block)+1)+*get_block_size(current_block);
+            return reinterpret_cast<void*>(get_pointer_to_past_block(reinterpret_cast<unsigned char*>(get_pointer_to_past_block(current_block)+1)+*get_block_size(current_block))+1);
+        }
+        return nullptr;
+
+    }
+    void deallocate(void* result) const override{
+        if (result== nullptr){
+            std::cout<<"cdc"<<std::endl;
+        }
+        auto block=reinterpret_cast<void*>(reinterpret_cast<unsigned char*>(result)-2*sizeof(void*)-sizeof(size_t));
+        size_t size= *get_block_size(block);
+        if (*get_pointer_to_past_block(block)==nullptr && *get_pointer_to_next_block(block)== nullptr){
+            *get_pointer()= nullptr;
+        }else if (*get_pointer_to_past_block(block)==nullptr && *get_pointer_to_next_block(block)!= nullptr){
+            *get_pointer_to_past_block(*get_pointer_to_next_block(block))= nullptr;
+            *get_pointer()=*get_pointer_to_next_block(block);
+
+        }else if (*get_pointer_to_next_block(block)== nullptr && *get_pointer_to_past_block(block)!= nullptr){
+            *get_pointer_to_next_block(*get_pointer_to_past_block(block))= nullptr;
+        }else{
+            std::cout<<*get_pointer_to_past_block(block)<< " " <<*get_pointer_to_next_block(block)<<std::endl;
+            *get_pointer_to_past_block(*get_pointer_to_next_block(block))=*get_pointer_to_past_block(block);
+            *get_pointer_to_next_block(*get_pointer_to_past_block(block))=*get_pointer_to_next_block(block);
+        }
+        std::string result_log = "[ ";
+        for (int i = 0; i < size; i++){
+            unsigned char a = *(reinterpret_cast<unsigned char*>(result) + i);
+            result_log+=  std::to_string(static_cast<unsigned short>(a));
+            result_log+=" ";
+        }
+        std::string result_msg = "Block to deallocate: " + result_log + "]";
+        if(*get_logger()!= nullptr){
+            (*get_logger())->log(result_msg, logger::severity::debug);
+
+        }
+
+    }
+
+
+    ~allocator_border_descriptor(){
+        if (*get_allocator()== nullptr){
+            ::operator delete(_global_memory);
+        }else{
+            (*get_allocator())->deallocate(_global_memory);
+        }
+
+    }
+
+};
+
+#endif //LABS_4_SEM_ALLOCATOR_BORDER_DESCRIPTOR_H
